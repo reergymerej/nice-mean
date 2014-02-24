f096adbdb648435cf9437cf21f157cb85f1e680f
Jeremy Greer <jeremygreer@reergymerej.com>
2014-02-23 22:13:47 -0500

    Add comments from entry point of index request to header.html.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
 exports.render = function(req, res) {
+
+    // Render the index view - /app/views/index.html
+    // The path and extension are not necessary - they 
+    // must be defined elsewhere.
+    // GOTO: /app/views/index.html
+    // REF: http://expressjs.com/api.html#res.render
     res.render('index', {
         user: req.user ? JSON.stringify(req.user) : 'null'
     });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,13 @@
 module.exports = function(app) {
     
     // Home route
+    // Load the index controller.
     var index = require('../controllers/index');
+
+    // When the '/' path is requested,
+    // execute the "render" method of the index controller.
+    // GOTO: /app/controllers/index:render
+    // REF: http://expressjs.com/api.html#app.get
     app.get('/', index.render);
 
 };
