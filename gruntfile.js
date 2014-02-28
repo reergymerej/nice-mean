6c5b34175e70141e84992721ae054601f02a5441
Jeremy Greer <jeremygreer@reergymerej.com>
2014-02-27 22:04:32 -0500

    Document gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,15 @@
 'use strict';
 
+// When "grunt" is run from the root, this gruntfile.js is picked up.
+// Grunt is used to automate repetitive tasks and has fabulous docs.
+// REF: http://gruntjs.com/getting-started#how-the-cli-works
+// REF: http://gruntjs.com/getting-started#the-gruntfile
+
+// REF: http://gruntjs.com/getting-started#the-wrapper-function
 module.exports = function(grunt) {
     // Project Configuration
+    // REF: http://gruntjs.com/getting-started#project-and-task-configuration
+    // You can run most of these by specifying them after "grunt" in terminal.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         watch: {
@@ -25,6 +33,9 @@ module.exports = function(grunt) {
                 }
             }
         },
+
+        // Run jshint.
+        // example: grunt jshint
         jshint: {
             all: {
                 src: ['gruntfile.js', 'server.js', 'app/**/*.js', 'public/js/**', 'test/**/*.js', '!test/coverage/**/*.js'],
@@ -55,6 +66,9 @@ module.exports = function(grunt) {
                 logConcurrentOutput: true
             }
         },
+
+        // Run Mocha tests.
+        // QUESTION: What's Mocha?
         mochaTest: {
             options: {
                 reporter: 'spec',
@@ -67,6 +81,9 @@ module.exports = function(grunt) {
                 NODE_ENV: 'test'
             }
         },
+
+        // Run Karma tests.
+        // QUESTION: What's Karma?
         karma: {
             unit: {
                 configFile: 'test/karma/karma.conf.js'
@@ -75,6 +92,7 @@ module.exports = function(grunt) {
     });
 
     //Load NPM tasks
+    // REF: http://gruntjs.com/getting-started#loading-grunt-plugins-and-tasks
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
@@ -86,6 +104,7 @@ module.exports = function(grunt) {
     //Making grunt default to force in order not to break the project.
     grunt.option('force', true);
 
+    // REF: http://gruntjs.com/getting-started#custom-tasks
     //Default task(s).
     grunt.registerTask('default', ['jshint', 'concurrent']);
 
